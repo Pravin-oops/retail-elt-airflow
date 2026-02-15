bcf795d112760064c9e929e58cf3cd99fe3916f3
Pravin <pravin.puducherry@gmail.com>
2026-02-15 20:30:05 +0530

    sql modified to run by dag

--- sql/init_oracle/02_directory_creation.sql
+++ sql/init_oracle/02_directory_creation.sql
@@ -2,5 +2,4 @@
 -- Create a logical reference to the physical folder inside the Docker container
 -- Note: /data is the path we defined in docker-compose.yml
 CREATE OR REPLACE DIRECTORY source_data_dir AS '/data';
--- Verify access (This checks if Oracle can "see" the folder)
-SELECT * FROM all_directories WHERE directory_name = 'SOURCE_DATA_DIR';
+/
